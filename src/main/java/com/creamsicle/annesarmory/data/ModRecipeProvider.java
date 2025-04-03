06b3f33944fd862e94624083aca6bcae6cd873ea
Creamsicle42 <defuego424242@gmail.com>
2025-04-03 11:15:47 -0700

    Setup upgrades for all stone tier items

--- src/main/java/com/creamsicle/annesarmory/data/ModRecipeProvider.java
+++ src/main/java/com/creamsicle/annesarmory/data/ModRecipeProvider.java
@@ -6,13 +6,13 @@ import net.minecraft.advancements.Criterion;
 import net.minecraft.advancements.critereon.SimpleCriterionTrigger;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.recipes.RecipeCategory;
-import net.minecraft.data.recipes.RecipeOutput;
-import net.minecraft.data.recipes.RecipeProvider;
-import net.minecraft.data.recipes.SmithingTransformRecipeBuilder;
+import net.minecraft.data.recipes.*;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.BlastingRecipe;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 
 import java.util.concurrent.CompletableFuture;
@@ -28,16 +28,116 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
     @Override
     protected void buildRecipes(RecipeOutput pRecipeOutput) {
 
-        SmithingTransformRecipeBuilder.smithing(
-                        Ingredient.of(ModItems.REFINED_IRON_UPGRADE_TEMPLATE.get()),
-                        Ingredient.of(Items.STONE_SWORD),
-                        Ingredient.of(ModItems.REFINED_IRON_INGOT.get()),
-                        RecipeCategory.TOOLS,
-                        Items.IRON_SWORD
-                ).unlocks("criteria", has(ModItems.REFINED_IRON_UPGRADE_TEMPLATE.get()))
-                .save(pRecipeOutput, "stone_sword_transmute");
+        SimpleCookingRecipeBuilder.blasting(
+                Ingredient.of(Items.IRON_INGOT),
+                RecipeCategory.MISC,
+                ModItems.REFINED_IRON_NUGGET.get(),
+                2,
+                200
+        )
+                .unlockedBy("criteria", has(Items.IRON_INGOT))
+                .save(pRecipeOutput, "refined_iron_blasting");
+
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.REFINED_IRON_INGOT.get())
+                .pattern("aaa")
+                .pattern("aaa")
+                .pattern("aaa")
+                .define('a', ModItems.REFINED_IRON_NUGGET.get())
+                .unlockedBy("criteria", has(ModItems.REFINED_IRON_NUGGET.get()))
+                .save(pRecipeOutput);
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.REFINED_IRON_NUGGET.get(), 9)
+                .requires(ModItems.REFINED_IRON_INGOT.get())
+                .unlockedBy("criteria", has(ModItems.REFINED_IRON_INGOT.get()))
+                .save(pRecipeOutput);
+
+
+
+        smithingUpgrade(
+                ModItems.REFINED_IRON_UPGRADE_TEMPLATE.get(),
+                Items.STONE_SWORD,
+                Items.IRON_SWORD,
+                ModItems.REFINED_IRON_INGOT.get(),
+                "stone_sword_upgrade",
+                pRecipeOutput
+        );
+        smithingUpgrade(
+                ModItems.REFINED_IRON_UPGRADE_TEMPLATE.get(),
+                Items.STONE_AXE,
+                Items.IRON_AXE,
+                ModItems.REFINED_IRON_INGOT.get(),
+                "stone_axe_upgrade",
+                pRecipeOutput
+        );
+        smithingUpgrade(
+                ModItems.REFINED_IRON_UPGRADE_TEMPLATE.get(),
+                Items.STONE_SHOVEL,
+                Items.IRON_SHOVEL,
+                ModItems.REFINED_IRON_INGOT.get(),
+                "stone_shovel_upgrade",
+                pRecipeOutput
+        );
+        smithingUpgrade(
+                ModItems.REFINED_IRON_UPGRADE_TEMPLATE.get(),
+                Items.STONE_HOE,
+                Items.IRON_HOE,
+                ModItems.REFINED_IRON_INGOT.get(),
+                "stone_hoe_upgrade",
+                pRecipeOutput
+        );
+        smithingUpgrade(
+                ModItems.REFINED_IRON_UPGRADE_TEMPLATE.get(),
+                Items.STONE_PICKAXE,
+                Items.IRON_PICKAXE,
+                ModItems.REFINED_IRON_INGOT.get(),
+                "stone_pickaxe_upgrade",
+                pRecipeOutput
+        );
+        smithingUpgrade(
+                ModItems.REFINED_IRON_UPGRADE_TEMPLATE.get(),
+                Items.LEATHER_BOOTS,
+                Items.IRON_BOOTS,
+                ModItems.REFINED_IRON_INGOT.get(),
+                "leather_boots_upgrade",
+                pRecipeOutput
+        );
+        smithingUpgrade(
+                ModItems.REFINED_IRON_UPGRADE_TEMPLATE.get(),
+                Items.LEATHER_LEGGINGS,
+                Items.IRON_LEGGINGS,
+                ModItems.REFINED_IRON_INGOT.get(),
+                "leather_leggings_upgrade",
+                pRecipeOutput
+        );
+        smithingUpgrade(
+                ModItems.REFINED_IRON_UPGRADE_TEMPLATE.get(),
+                Items.LEATHER_CHESTPLATE,
+                Items.IRON_CHESTPLATE,
+                ModItems.REFINED_IRON_INGOT.get(),
+                "leather_chestplate_upgrade",
+                pRecipeOutput
+        );
+        smithingUpgrade(
+                ModItems.REFINED_IRON_UPGRADE_TEMPLATE.get(),
+                Items.LEATHER_HELMET,
+                Items.IRON_HELMET,
+                ModItems.REFINED_IRON_INGOT.get(),
+                "leather_helmet_upgrade",
+                pRecipeOutput
+        );
 
     }
 
+    private static void smithingUpgrade(Item template, Item base, Item result, Item otherIng, String name, RecipeOutput pRecipeOutput) {
+        SmithingTransformRecipeBuilder.smithing(
+                        Ingredient.of(template),
+                        Ingredient.of(base),
+                        Ingredient.of(otherIng),
+                        RecipeCategory.TOOLS,
+                        result
+                ).unlocks("criteria", has(template))
+                .save(pRecipeOutput, name);
+    }
 
 }
